a9f29593701b6af854014f3068e5c15cb3acb091
Fubar <fubar2k19@gmail.com>
2019-02-28 22:30:26 +0530

    New Site

--- Template/cafe/edit.php
+++ Template/cafe/edit.php
@@ -123,7 +123,7 @@ body, html {
 
 <!-- Header with image -->
 <!-- Header with image -->
-<br><br><br><br><br><br>
+<br><br><br><br><br><br><br>
 <header class="w3-grayscale-min" id="home">
   <input class="w3-display-center" type="file" name="file9" id="file9">
   <div class=" w3-center w3-display-bottomleft w3-padding-large w3-hide-small">
