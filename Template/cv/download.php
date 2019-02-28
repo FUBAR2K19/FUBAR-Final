65d93886fcaf83f5d6db51ceaebf6934f9d1d317
Fubar <fubar2k19@gmail.com>
2019-02-28 20:31:52 +0530

    New Site

--- Template/cv/download.php
+++ Template/cv/download.php
@@ -6,6 +6,6 @@ $url = $_GET['url'];
 echo '<a href='.$url.'> Visit your page</a>';
   $file1 = '../cv/cv_files/file1.jpg';
   unlink($file1);
-file_put_contents("../constructive1/home.html", "");
+file_put_contents("../cv1/home.html", "");
 
  ?>
