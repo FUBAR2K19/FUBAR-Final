14ba1fbe2e5601056c3003d2f806906b46c4fc07
Fubar <fubar2k19@gmail.com>
2019-02-28 22:25:42 +0530

    New Site

--- Template/cafe/edit.php
+++ Template/cafe/edit.php
@@ -124,6 +124,7 @@ body, html {
 <!-- Header with image -->
 <!-- Header with image -->
 <header class="bgimg w3-display-container w3-grayscale-min" id="home">
+  <input type="file" name="file9" id="file9">
   <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
     <span class="w3-tag"><input name="time1" placeholder="Cafe Time" size=""></input></span>
   </div>
