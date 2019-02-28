fd86f275d23291deb6129dc1599e078fc4a6f27f
Fubar <fubar2k19@gmail.com>
2019-02-28 20:31:41 +0530

    New Site

--- Template/cv/download.php
+++ Template/cv/download.php
@@ -1,7 +1,7 @@
 <?php
 
 
-echo '<a href="cons.zip" download>download source code</a>';
+echo 'check mail for source code';
 $url = $_GET['url'];
 echo '<a href='.$url.'> Visit your page</a>';
   $file1 = '../cv/cv_files/file1.jpg';
